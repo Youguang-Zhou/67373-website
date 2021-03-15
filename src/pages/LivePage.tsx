672a2f0cb35cc7eac7614e07fbdfac59f5fc590e
Youguang-Zhou <youguang.zhou@icloud.com>
2021-03-16 04:24:13 +0800

    feat: add LivePage

--- src/pages/LivePage.tsx
+++ src/pages/LivePage.tsx
@@ -1,5 +1,35 @@
-import React, { FC } from 'react'
+import { Col, Row } from 'antd'
+import React, { FC, useEffect, useState } from 'react'
+import { getLiveURL } from '../api'
+import VideoPlayer from '../components/VideoPlayer'
+import { IPlayOptions } from '../interfaces/index'
 
-const LivePage: FC = () => <div>LivePage</div>
+const LivePage: FC = () => {
+	const [playOptions, setPlayOptions] = useState<IPlayOptions | null>(null)
+
+	useEffect(() => {
+		getLiveURL().then((url) => {
+			const options = {
+				sources: [
+					{
+						src: url,
+						type: 'application/x-mpegURL',
+					},
+				],
+			}
+			setPlayOptions(options)
+		})
+	}, [])
+
+	return (
+		<main className="mt-3">
+			<Row justify="center">
+				<Col xs={24} sm={24} md={24} lg={16} xl={16}>
+					{playOptions && <VideoPlayer options={playOptions} />}
+				</Col>
+			</Row>
+		</main>
+	)
+}
 
 export default LivePage
