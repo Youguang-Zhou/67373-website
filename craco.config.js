605afba522bc1030706d14dde37c17d8698516d1
Youguang-Zhou <youguang.zhou@outlook.com>
2021-03-27 02:56:45 +0800

    chore: update packages

--- craco.config.js
+++ craco.config.js
@@ -2,20 +2,6 @@
 const CracoLessPlugin = require('craco-less')
 
 module.exports = {
-	/**
-	 * 因为这个文件夹是放在iCloud里的，为了避免同步node_modules，执行了：
-	 * 		mkdir node_modules.nosync && ln -s node_modules.nosync node_modules
-	 * 的命令，创建了node_modules.nosync的软连接
-	 */
-	webpack: process.env.NODE_ENV === 'development' && {
-		configure: (webpackConfig) => {
-			const scopePluginIndex = webpackConfig.resolve.plugins.findIndex(
-				({ constructor }) => constructor && constructor.name === 'ModuleScopePlugin'
-			)
-			webpackConfig.resolve.plugins.splice(scopePluginIndex, 1)
-			return webpackConfig
-		},
-	},
 	plugins: [
 		{
 			plugin: CracoLessPlugin,
