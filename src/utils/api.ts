3ddc6ea2f4823a90fc68786d51dce8fb315b4b9e
Youguang-Zhou <youguang.zhou@icloud.com>
2021-04-22 00:47:12 +0800

    feat(LivePage): add counter for live time

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -12,11 +12,10 @@ interface IVideoPlayDetail {
 	videoBase: { title: string }
 }
 
+const { NODE_ENV, REACT_APP_API_BASE_URL_PROD, REACT_APP_API_BASE_URL_DEV, REACT_APP_LIVE_TIME_URL } = process.env
+
 const API = axios.create({
-	baseURL:
-		process.env.NODE_ENV === 'production'
-			? process.env.REACT_APP_API_BASE_URL_PROD
-			: process.env.REACT_APP_API_BASE_URL_DEV,
+	baseURL: NODE_ENV === 'production' ? REACT_APP_API_BASE_URL_PROD : REACT_APP_API_BASE_URL_DEV,
 })
 
 const getVideoList = async (cateId: number | undefined, pageNo = 1, pageSize = 12): Promise<IVideoList> => {
@@ -31,4 +30,9 @@ const getPlayURL = (id: string): Promise<IVideoPlayDetail> => API.get(`videos/${
 
 const getLiveURL = (): Promise<{ liveURL: string }> => API.get('live').then(({ data }) => data)
 
-export { getVideoList, getPlayURL, getLiveURL }
+// 需要阿里云oss开启跨域
+const getLiveTime = (): Promise<{ time: string; url: string; cover: string }> =>
+	// eslint-disable-next-line
+	API.get(REACT_APP_LIVE_TIME_URL!).then(({ data }) => data)
+
+export { getVideoList, getPlayURL, getLiveURL, getLiveTime }
