8bb83d80aae75fa9af83ea146ab765638e0635ab
Youguang-Zhou <youguang.zhou@icloud.com>
2021-06-15 16:18:16 +0800

    feat: add AudioCardSmall

--- src/components/AudioList.tsx
+++ src/components/AudioList.tsx
@@ -3,6 +3,7 @@ import { LyricContext } from '../contexts/LyricContext'
 import { MusicContext } from '../contexts/MusicContext'
 import { IVod } from '../utils/interfaces'
 import AudioCard from './AudioCard'
+import AudioCardSmall from './AudioCardSmall'
 
 const AudioList: FC = () => {
 	const { currSong, playlist, getCurrSource, playAudioById } = useContext(MusicContext)
@@ -18,17 +19,35 @@ const AudioList: FC = () => {
 	}
 
 	return (
-		<section className="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-6 g-5">
-			{playlist &&
-				playlist.map((audio: IVod) => (
-					<AudioCard
-						key={audio.videoId}
-						audio={audio}
-						highlight={isClickCurrSong(audio)}
-						onDoubleClick={() => handleDoubleClick(audio)}
-					/>
-				))}
-		</section>
+		<>
+			{/* 桌面端尺寸 */}
+			<section className="d-none d-sm-flex row row-cols-sm-3 row-cols-md-4 row-cols-lg-6 g-5">
+				{playlist &&
+					playlist.map((audio: IVod) => (
+						<AudioCard
+							key={audio.videoId}
+							audio={audio}
+							highlight={isClickCurrSong(audio)}
+							onDoubleClick={() => handleDoubleClick(audio)}
+						/>
+					))}
+			</section>
+			{/* 移动端尺寸 */}
+			<section className="container d-block d-sm-none">
+				{playlist &&
+					playlist.map((audio: IVod) => (
+						<>
+							<AudioCardSmall
+								key={audio.videoId}
+								audio={audio}
+								highlight={isClickCurrSong(audio)}
+								onDoubleClick={() => handleDoubleClick(audio)}
+							/>
+							<hr className="mx-0 my-0" />
+						</>
+					))}
+			</section>
+		</>
 	)
 }
 
