98b818d6fd3a25b0d4c2ec565cad5c0197bfe5a0
Youguang-Zhou <youguang.zhou@outlook.com>
2021-07-02 16:43:04 +0800

    refactor(Tailwind CSS): Header and SearchBar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,90 +1,84 @@
+import CloseRoundedIcon from '@material-ui/icons/CloseRounded'
+import MenuRoundedIcon from '@material-ui/icons/MenuRounded'
 import NotificationsNoneRoundedIcon from '@material-ui/icons/NotificationsNoneRounded'
 import YouTubeIcon from '@material-ui/icons/YouTube'
-import { Row } from 'antd'
 import React, { FC, useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
-import styled from 'styled-components'
 import logo from '../assets/images/logo_v2.jpg'
 import useGetLiveInfoRequest from '../hooks/useGetLiveInfoRequest'
 import { LiveStatus } from '../utils/enums'
 import SearchBar from './SearchBar'
 
-const Logo = styled.img`
-	height: 56.5px;
-	width: 233.2px;
-
-	@media (max-width: 360px) {
-		height: 45.2px;
-		width: 186.56px;
-	}
-`
+const navItems = [
+	{ name: '首页', path: '/' },
+	{ name: '频道', path: '/channel' },
+	{ name: '音乐', path: '/music' },
+	{ name: '67373', path: '/67373' },
+]
 
 const Header: FC = () => {
+	const [currPath, setCurrPath] = useState<string>('')
+	const [toggle, setToggle] = useState<boolean>(false)
 	const { pathname } = useLocation()
-	const [currNav, setCurrNav] = useState('')
 	const {
 		response: { status },
 	} = useGetLiveInfoRequest()
 
 	useEffect(() => {
-		setCurrNav(pathname)
+		setCurrPath(pathname)
 	}, [pathname])
 
-	return (
-		<>
-			{currNav && (
-				<nav className="navbar navbar-expand-lg navbar-light">
-					<div className="container-fluid">
-						<a className="navbar-brand" href="/">
-							<Logo src={logo} alt="chenyifaer" />
-						</a>
-						<button
-							className="navbar-toggler"
-							type="button"
-							data-bs-toggle="collapse"
-							data-bs-target="#BootstrapNavbar"
-							aria-controls="BootstrapNavbar"
-							aria-expanded="false"
-							aria-label="Toggle navigation"
-						>
-							<span className="navbar-toggler-icon"></span>
-						</button>
-						<div className="collapse navbar-collapse" id="BootstrapNavbar">
-							<div className="navbar-nav text-center fs-4 me-auto">
-								<a className={`nav-link ${currNav === '/' && 'active'} px-4 py-1`} href="/">
-									首页
-								</a>
-								<a
-									className={`nav-link ${currNav === '/channel' && 'active'} px-4 py-1`}
-									href="/channel"
-								>
-									频道
-								</a>
-								<a
-									className={`nav-link ${currNav.startsWith('/music') && 'active'} px-4 py-1`}
-									href="/music"
-								>
-									音乐
-								</a>
-								<a className={`nav-link ${currNav === '/67373' && 'active'} px-4 py-1`} href="/67373">
-									<Row align="middle" justify="center">
-										<span>67373</span>
-										{status === LiveStatus.WillStart && (
-											<NotificationsNoneRoundedIcon style={{ color: '#164080c1' }} />
-										)}
-										{status === LiveStatus.IsLive && (
-											<YouTubeIcon style={{ color: '#164080c1' }} fontSize="large" />
-										)}
-									</Row>
-								</a>
-							</div>
-							<SearchBar />
-							<div className="me-auto"></div>
-						</div>
+	const renderNavItems = () =>
+		navItems.map(({ name, path }, index) => (
+			<a
+				key={index}
+				className={`${
+					currPath === path ? 'text-gray-900 hover:text-gray-900' : 'hover:text-gray-700'
+				} text-xl md:text-2xl text-gray-500 p-3`}
+				href={path}
+			>
+				{/* 如果是67373页面后面会附上直播状态的图标 */}
+				{name === '67373' ? (
+					<div className="flex items-center justify-center space-x-0.5">
+						<span>{name}</span>
+						{status === LiveStatus.WillStart && <NotificationsNoneRoundedIcon className="text-primary" />}
+						{status === LiveStatus.IsLive && <YouTubeIcon className="text-primary" />}
 					</div>
+				) : (
+					<span>{name}</span>
+				)}
+			</a>
+		))
+
+	return (
+		<header className="px-1 md:px-4">
+			{/* 桌面端 */}
+			<div className="flex items-center justify-between h-16 md:h-20 md:justify-start">
+				{/* logo */}
+				<a className="flex-shrink-0" href="/">
+					<img className="h-12 md:h-14" src={logo} alt="ChenYiFaer" />
+				</a>
+				{/* 桌面端的导航栏 */}
+				<nav className="items-center justify-center flex-shrink-0 hidden ml-0 md:flex md:space-x-1 lg:space-x-4 lg:ml-8">
+					{renderNavItems()}
 				</nav>
-			)}
-		</>
+				{/* 桌面端的搜索栏 */}
+				<SearchBar className="flex-grow hidden max-w-xs ml-0 md:block lg:ml-32" />
+				{/* 移动端logo右侧显示导航栏的按钮 */}
+				<button
+					type="button"
+					className="block text-gray-400 md:hidden hover:text-gray-500"
+					onClick={() => setToggle(!toggle)}
+				>
+					{toggle ? <CloseRoundedIcon fontSize="large" /> : <MenuRoundedIcon fontSize="large" />}
+				</button>
+			</div>
+			{/* 移动端 */}
+			<div className={`block overflow-hidden transition-all ${toggle ? 'max-h-32' : 'max-h-0'} md:hidden`}>
+				<nav className="flex items-center justify-evenly">{renderNavItems()}</nav>
+				<SearchBar className="mb-3" />
+			</div>
+		</header>
 	)
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
 	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
 	darkMode: false,
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#174080',
+			},
+		},
 	},
 	variants: {
 		extend: {},
