758db5b58010b85aa86f7591c25f44a44361c676
Youguang-Zhou <youguang.zhou@outlook.com>
2021-07-04 23:47:07 +0800

    chore: remove unused packages

--- craco.config.js
+++ craco.config.js
@@ -1,23 +1,7 @@
-/* eslint-disable */
-const CracoLessPlugin = require('craco-less')
-
 module.exports = {
 	style: {
 		postcss: {
 			plugins: [require('tailwindcss'), require('autoprefixer')],
 		},
 	},
-	plugins: [
-		{
-			plugin: CracoLessPlugin,
-			options: {
-				lessLoaderOptions: {
-					lessOptions: {
-						modifyVars: { '@primary-color': '#174080' },
-						javascriptEnabled: true,
-					},
-				},
-			},
-		},
-	],
 }
