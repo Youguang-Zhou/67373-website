1ec40abed1238f775249ca2c8be93b5d3eec09d7
Youguang-Zhou <youguang.zhou@outlook.com>
2021-07-04 16:53:05 +0800

    refactor(Tailwind CSS): Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,26 +7,19 @@ const Footer: FC = () => {
 
 	return (
 		<footer
-			className="text-center py-3"
-			style={{
-				backgroundColor: pathname.startsWith('/music') ? '#121212' : 'white',
-				color: pathname.startsWith('/music') ? '#fafafafa' : '#00000080',
-			}}
+			className={`flex flex-col sm:flex-row items-center justify-center py-4 text-center ${
+				pathname.startsWith('/music') ? 'bg-spotify-900 text-light' : 'bg-white text-black text-opacity-50'
+			}`}
 		>
-			<span className="px-3">{`© 2020-${new Date().getFullYear()} 67373upup.com`}</span>
-			<span className="px-3 d-block d-sm-inline-block">
-				<a
-					className="text-reset text-decoration-none"
-					href="http://beian.miit.gov.cn/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
+			<span className="px-4">{`© 2020-${new Date().getFullYear()} 67373upup.com`}</span>
+			<span className="px-4">
+				<a href="http://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">
 					苏ICP备20007695号-1
 				</a>
 			</span>
-			{/* <span className="px-3 d-block d-sm-inline-block">
+			{/* <span className="px-4">
 				<a
-					className="text-reset text-decoration-none"
+					className="flex items-center justify-center"
 					href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=32059002002904"
 					target="_blank"
 					rel="noopener noreferrer"
