deeff71d53b1fa9bd4d14a04a87dfac07233e6c5
Youguang-Zhou <youguang.zhou@outlook.com>
2021-07-07 13:35:52 +0800

    fix(InfiniteScroll): fix load more behavior

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,7 +8,7 @@ import { MediaType } from '../utils/enums'
 import { VodProps } from '../utils/interfaces'
 
 const HomePage: FC = () => {
-	const [videos, setVideos] = useState<VodProps[]>([])
+	const [videos, setVideos] = useState<VodProps[] | undefined>(undefined)
 	const [pageNo, setPageNo] = useState(1)
 	const pageSize = useState(12)[0]
 	const {
@@ -19,7 +19,7 @@ const HomePage: FC = () => {
 	} = useGetVodListRequest(MediaType.Video, pageNo, pageSize)
 
 	useEffect(() => {
-		videoList && setVideos(videos.concat(videoList.video))
+		videoList && setVideos(videos?.concat(videoList.video) || videoList.video)
 	}, [requestId])
 
 	const handleLoadMore = () => setPageNo((prevPageNo) => prevPageNo + 1)
@@ -28,19 +28,21 @@ const HomePage: FC = () => {
 		<main className="p-container bg-whitesmoke">
 			<h1 className="text-2xl md:text-4xl">今日推荐</h1>
 			<hr className="my-2 md:my-4" />
-			<InfiniteScroll
-				hasMore={hasMore}
-				hasError={hasError}
-				isLoading={isLoading}
-				loadMore={handleLoadMore}
-				emptyComponent={<Empty image={love_you} />}
-			>
-				<section className="video-container">
-					{videos.map((video) => (
-						<VideoCard key={video.videoId} video={video} />
-					))}
-				</section>
-			</InfiniteScroll>
+			{videos && (
+				<InfiniteScroll
+					hasMore={hasMore}
+					hasError={hasError}
+					isLoading={isLoading}
+					loadMore={handleLoadMore}
+					emptyComponent={<Empty image={love_you} />}
+				>
+					<section className="video-container">
+						{videos.map((video) => (
+							<VideoCard key={video.videoId} video={video} />
+						))}
+					</section>
+				</InfiniteScroll>
+			)}
 		</main>
 	)
 }
