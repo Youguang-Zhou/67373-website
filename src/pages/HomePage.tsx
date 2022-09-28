8987a42eef64eaa4efb25aea5296329b4105e3d5
Youguang-Zhou <youguang.zhou@outlook.com>
2022-09-28 18:41:36 +0100

    feat(HomePage): add Easter eggs for birthday

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { useInfiniteQuery } from 'react-query'
 import InfiniteScroll from '../components/InfiniteScroll'
 import VideoCard from '../components/VideoCard'
@@ -17,12 +18,28 @@ const HomePage = () => {
 		getNextPageParam: ({ nextPage }) => nextPage,
 	})
 
+	const [isBirthday, setIsBirthday] = useState<boolean | undefined>()
+
+	useEffect(() => {
+		const month = new Date().getMonth() + 1
+		const date = new Date().getDate()
+		setIsBirthday(month == 10 && date == 13)
+	}, [])
+
 	return (
 		<main className="main-container bg-whitesmoke">
-			<h1 className="text-2xl md:text-4xl">今日推荐</h1>
+			<h1 className="text-2xl md:text-4xl">
+				今日推荐 <span>{isBirthday && '🎂'}</span>
+			</h1>
 			<hr className="mt-2 md:mt-4" />
 			<div className="mb-2 md:mb-4 text-gray-700">
-				公告：最新回放请关注微博超话：陈一发儿超话。本站催更联系微博@青山多妩媚67373，谢谢大家支持：）
+				{isBirthday ? (
+					<span>67373代码部祝人类高质量台主、游戏高手陈一发儿生日快乐～</span>
+				) : (
+					<span>
+						公告：最新回放请关注微博超话：陈一发儿超话。本站催更联系微博@青山多妩媚67373，谢谢大家支持：）
+					</span>
+				)}
 			</div>
 			<InfiniteScroll
 				hasMore={hasNextPage}
